7b6c3e94743cd1a034945f49bf389180b68c68e8
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2023-06-17 22:19:13 +0700

    remove dormant data processing

--- markdown/compile.py
+++ markdown/compile.py
@@ -13,6 +13,7 @@ import pandas as pd
 books = pd.read_csv("https://raw.githubusercontent.com/kreier/study/main/markdown/chapters.csv")
 # print(books.info())
 
+
 # Process one book by one
 
 text_markdown =""
@@ -44,26 +45,4 @@ for index, row in books.iterrows():
         total_output_errors += 1
     text_markdown = ""  
 
-exit
-
-for index in range(50):
-    filename = f'01_Ge/{index:02d}.md'
-    try:
-        with open(filename, 'r') as input:
-            file_data = input.read()
-            words = file_data.split(" ")
-            num_words = len(words)
-            # logging.debug(f"{filename} has {num_words} words")
-            total_words += num_words
-            total_chapters += 1
-            text_markdown += file_data
-        text_markdown += '\n'
-    except OSError as e:
-        total_import_errors += 1
-        # logging.error(f"error reading {filename}")
-        pass
-
 logging.debug(f"""Processed {total_chapters} chapters. That's {total_chapters / 1189 * 100:.1f} Percent. They contain {total_words} words. {total_import_errors} import errors. {total_output_errors} output errors.""")
-
-# with open('05_Ge/README.md', 'w') as output:
-#     output.write(text_markdown)
